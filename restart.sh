1f7959b32be215116f3042a32b3b4bf888f41302
Magnus Manske <magnusmanske@googlemail.com>
2026-07-30 12:38:41 +0300

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 toolforge jobs delete bot
-toolforge jobs run --mem 3G --cpu 2 --continuous --mount=all --image tool-quickstatements/tool-quickstatements:latest --command "sh -c 'target/release/main --command bot --config-file /data/project/quickstatements/rust/quickstatements_rs/config_rs.json'" bot
+\rm ~/bot.*
+toolforge jobs run --mem 3500Mi --cpu 2 --continuous \
+	--mount=all \
+	--image tool-quickstatements/tool-quickstatements:latest \
+	--command "target/release/main --command bot --config-file /data/project/quickstatements/rust/quickstatements_rs/config_rs.json" \
+	--filelog -o /data/project/quickstatements/bot.out -e /data/project/quickstatements/bot.err \
+	bot
